37fbd5d2588d9a34adac95a1ce2f6ea3ae122ff3
Angel Conde <neuw84@gmail.com>
2025-03-25 10:18:50 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,14 +2,14 @@
 
 # Wait for services to be ready
 echo "Waiting for services to be ready..."
-sleep 3
+sleep 30
 
 # Register the Postgres source connector
 echo "Registering Postgres connector..."
-curl -X POST -H "Content-Type: application/json" --data @register-postgres-connector.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" --data @connectors/register-postgres-connector.json http://localhost:8083/connectors
 
 # Register the Iceberg sink connector
  
 echo "Registering Iceberg sink connector..."
-curl -X POST -H "Content-Type: application/json" --data @iceberg/register-iceberg-sink.json http://localhost:8084/connectors
+curl -X POST -H "Content-Type: application/json" --data @connectors/register-iceberg-sink.json http://localhost:8084/connectors
 
